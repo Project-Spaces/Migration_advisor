bd1e81995f8181028bf19128cddb1263cb66afed
Maigacribzz <elebemaiga@yahoo.com>
2026-08-11 13:59:04 -0400

    changes to environmental varibales, error fixes

--- src/app/api/assess/route.js
+++ src/app/api/assess/route.js
@@ -1,10 +1,10 @@
 import { BedrockRuntimeClient, InvokeModelCommand } from "@aws-sdk/client-bedrock-runtime";
 
 const client = new BedrockRuntimeClient({
-  region: process.env.REGION || process.env.AWS_REGION,
+  region: process.env.BEDROCK_REGION || process.env.AWS_REGION,
   credentials: {
-    accessKeyId: process.env.ACCESS_KEY_ID || process.env.AWS_ACCESS_KEY_ID,
-    secretAccessKey: process.env.SECRET_ACCESS_KEY || process.env.AWS_SECRET_ACCESS_KEY,
+    accessKeyId: process.env.BEDROCK_ACCESS_KEY || process.env.AWS_ACCESS_KEY_ID,
+    secretAccessKey: process.env.BEDROCK_SECRET_KEY || process.env.AWS_SECRET_ACCESS_KEY,
   },
 });
 
